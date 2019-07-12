fed8c948b5aa86e89ee42f3e654e4ad53bb8a0f7
AyRaiden <ayraidenx@gmail.com>
2019-07-12 17:04:07 +0100

    Test Graphique

--- src/com/company/esi/Main.java
+++ src/com/company/esi/Main.java
@@ -1,85 +1,29 @@
 package com.company.esi;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 import java.sql.Array;
 import java.util.*;
 
-public class Main {
+public class Main extends Application {
+
     public static void main(String[] args) {
-        ESIQUIZ application = new ESIQUIZ();
-        //deroulement
-        //creation compte formateur
-        application.creerCompteFormateur("form","pr","12081970");
-        //connexion au compte
-        application.seConecter("prform","form12081970",1);
-        Formateur f = (Formateur) application.compteConnecte;
-        //Creation d'un formation
-        f.creerFormation("POO","description","05062019","15062019");
-        //Creation des comptes des apprenant
-        f.creerCompteApprenant("boudis","madjid","08102000");
-        f.creerCompteApprenant("yahiaoui","ahmed","25121999");
-        //ajout d'un compte apprenant
-        f.creerCompteApprenant("derouas","noufel","08102000");
-        //suppression d'un compte apprenant
-        f.supprimerCompteApprenant("noufelderouas");
-        //modification d'un compte apprenant
-        f.modifierCompteApprenant("ahmedyahiaoui","ahmed1","yahiaoui99");
-        //afficher les comptes des apprenants
-        f.consulterActiviteApprenant();
+        launch(args);
+    }
 
-        //creation de notion question quiz
-
-        //creation 2 quiz
-        f.ajouterQuiz("quiz1","12052019","25122019");
-        f.ajouterQuiz("quiz2","12052019","25122019");
-        f.selectQuiz("quiz1");
-
-        //ajout notion
-        Notion notion=new Notion("notion1");
-        //ajout 3 question de la notion
-        List<String>  propo = new ArrayList<String>(Arrays.asList("propo1","propo2","propo3"));
-        List<Integer> projuste = new ArrayList<Integer>(Arrays.asList(1,2));
-        notion.ajouterQCM("QCM",propo,projuste);
-        notion.ajouterQCU("QCU",propo,2);
-        notion.ajouterQO("QO enonce","bonnerep");
-        notion.ajouterQO("QO2","bonnerep2");
-        //supression dune question
-        notion.suprimerQuestion(3);
-        //modifier question
-        notion.modifierQuestion(1,"nouvel enonce");
-        //supprimer quiz
-        f.supprimerQuiz("quiz2");
-        //ajout notion au quiz //quizTemp.genererQuiz(notion);
-        f.quizTemp.ajouterNotion(notion);
-
-        //sauvegarde pur l'appenant
-        f.ajoutQuizToApp();
-        //Connexion a un compte apprenant
-        f.seDeconnecter();
-        application.seConecter("ahmed1","yahiaoui99",0);
-        Apprenant apr = (Apprenant)application.compteConnecte;
-        //selectioner le quiz
-        Quiz q = apr.tabQuiz.get(f.formation.tabQuiz.indexOf(f.formation.SearchQuiz("quiz1")));
-
-        //l'apprenant repond au quiz
-        //affichage du quiz;
-        apr.visualiserQuiz(q);
-        //creation des reponses
-        ReponseQCU rqcu = new ReponseQCU(2);
-        ReponseQO rqo = new ReponseQO("bonnerep");
-        ReponseQCM rqcm = new ReponseQCM(new ArrayList<Integer>(Arrays.asList(1,2)));
-        List<Reponse>  listereponse = new ArrayList<Reponse>(Arrays.asList(rqcm,rqcu,rqo));
-        apr.repondreQuiz(q,listereponse);
-        //soummetre le quiz
-        q.soumettreQuiz();
-        //mettre a evaluation
-        apr.soumettreEvaluation(q);
-        f.formation.avoirTauxresultat();
-
-        //afficher activité d'un apprenant specifique
-        System.out.print("activité apprenant :");
-        f.consulterActiviteApprenant(apr);
-        //avoir le classement
-        System.out.println("Classement :");
-        f.formation.avoirClassement();
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.show();
     }
 }
